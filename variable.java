eebc148493a0588651c8542e77df94c6a85f7307
anand83n <156614503+anand83n@users.noreply.github.com>
2024-04-05 17:07:56 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,12 @@
+public class variable {
+    public static void main(String args[]) {
+
+        int ticketPrice = 600;
+        boolean paid = true;
+        char c = 't';
+        System.out.println(paid);
+        System.out.println(c);
+        System.out.println(" ticket price is "  +  ticketPrice  +  "  Rs  ");
+    }
+    
+}
