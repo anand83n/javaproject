391cb808a8404fa55b5251176dec2b5437e597fd
anand83n <156614503+anand83n@users.noreply.github.com>
2024-04-06 18:12:13 +0530

    typecasting

---
+++ typecasting.java
@@ -0,0 +1,15 @@
+/*
+ * Type Casting
+ */
+    public class typecasting {
+        public static void main(String args[]){
+
+            byte b = 5; //1 byte
+            int n; //4bytes
+            n = b; //explicit type conversion
+            System.out.println(b);
+
+        
+        }
+    }
+
