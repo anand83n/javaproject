aebbc988c4fab3586a8eea47f0c93f725db84042
anand83n <156614503+anand83n@users.noreply.github.com>
2024-04-08 23:12:36 +0530

    Update getinput.java

--- getinput.java
+++ getinput.java
@@ -7,7 +7,7 @@ public static void main (String args[]){
 
     try (Scanner scanner = new Scanner(System.in)) {
 
-        System.out.println("What is your name? ");
+        System.out.println("What is your name? "); 
 
         String name = scanner.nextLine();
 
