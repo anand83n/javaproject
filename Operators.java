ea408c1a9db9fd544b5324f13f28be22aad5d6e7
anand83n <156614503+anand83n@users.noreply.github.com>
2024-04-08 23:29:53 +0530

    arithmetic operators

---
+++ Operators.java
@@ -0,0 +1,21 @@
+
+/****************Arithmetic operators*****************/
+
+public class Operators {
+
+    public static void main(String[]args) {
+
+        // + - * / % 
+
+    int a = 10,b = 2,c;
+
+    c = a - b;
+
+    System.out.println(c);
+    }
+
+}
+
+
+    
+
