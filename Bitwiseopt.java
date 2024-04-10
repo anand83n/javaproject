6f36f37b2d278f5f368d690fac827a5e4363dbe2
anand83n <156614503+anand83n@users.noreply.github.com>
2024-04-10 22:27:10 +0530

    bitwise operator

---
+++ Bitwiseopt.java
@@ -0,0 +1,21 @@
+/**********Bitwise operators**************/
+
+public class Bitwiseopt {
+
+    public static void main(String args[]) {
+
+        int a=9,b=12,c;
+
+         c = a & b;
+        System.out.println("a&b is " + c );
+
+        c = a | b;
+        System.out.println("a|b is " + c );
+
+       c = ~a;
+        System.out.println("~a is " + c );
+    
+    }
+}
+    
+
