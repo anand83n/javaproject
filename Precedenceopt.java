17672792af926682e5097100ea7c890062ca1046
anand83n <156614503+anand83n@users.noreply.github.com>
2024-04-12 23:50:14 +0530

    operator precedence

---
+++ Precedenceopt.java
@@ -0,0 +1,14 @@
+/*********Operator Precedence***********/
+
+public class Precedenceopt {
+
+    public static void main(String[] args) {
+
+        int a=10,b=25,c;
+
+         c = a*b-10/ a;
+
+        System.out.println(c);
+
+    }
+}
