38b8e414a044fc8b36a41ec5aab4a773108996d8
anand83n <156614503+anand83n@users.noreply.github.com>
2024-07-27 23:45:07 +0530

    Add files via upload

---
+++ modifiers.java
@@ -0,0 +1,26 @@
+
+
+/* 
+1. Access modifiers : controls the access level.
+    class - public, default 
+    Attributes , method, Constructors - public , private , protected, default
+
+2. Non - Access modifiers : do not control access level, but provides other functionality class - final, abstract.
+*/
+ 
+
+public class modifiers {
+
+final static int x = 12;
+
+public static void main(String[] args) {
+
+System.out.println(x);
+
+}
+
+}
+
+    
+
+
