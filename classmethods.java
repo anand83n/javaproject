5d77e366afebbf7a0bdab24041fb281d0c48571f
anand83n <156614503+anand83n@users.noreply.github.com>
2024-07-29 22:32:16 +0530

    Add files via upload

---
+++ classmethods.java
@@ -0,0 +1,26 @@
+
+public class classmethods {
+
+    static void welcome() {
+
+        System.out.println("programmers");
+
+    }
+
+    public void text() {
+
+        System.out.println("programming in Tamil");
+
+    }
+
+    public static void main(String[] args) {
+
+        welcome();
+
+        classmethods main = new classmethods();
+
+        main.text();
+
+    }
+
+}
