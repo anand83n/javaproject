14868ace31a9268b83f6a14ed27660ba7b0efe78
anand83n <156614503+anand83n@users.noreply.github.com>
2024-07-31 23:36:27 +0530

    Add files via upload

---
+++ interface.java
@@ -0,0 +1,36 @@
+
+interface Father {
+
+    abstract void call();
+
+    abstract void talk();
+
+}
+
+class interface implements Father {
+
+
+public void call() {
+
+        System.out.println("Calling");
+
+    }
+
+    public void talk() {
+
+        System.out.println("Talking");
+
+    }
+
+    public static void main(String[] args) {
+
+        interface ob 
+        
+    
+
+ob.call();
+        ob.talk();
+
+    }
+
+}
