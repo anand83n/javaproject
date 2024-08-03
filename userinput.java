b03ff5375c90bb95e5a6ec954ff1d1dd7d2e4d76
anand83n <156614503+anand83n@users.noreply.github.com>
2024-08-03 14:21:55 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;       // package = collection of classes
+
+
+class userinput {
+
+public static void main(String[] args) {
+    
+Scanner input = new Scanner(System.in);
+
+System.out.println("Enter the value");
+
+
+String a = input.nextLine();
+
+
+System.out.println(a);
+
+
+}
+
+
+}
