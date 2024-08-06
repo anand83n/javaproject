df9db5ae7e080baaf38efda156405cc16217a9e2
anand83n <156614503+anand83n@users.noreply.github.com>
2024-08-06 23:35:50 +0530

    Add files via upload

---
+++ linkedlit.java
@@ -0,0 +1,26 @@
+
+import java.util.LinkedList;
+
+
+public class linkedlit {
+
+
+
+public static void main(String[] args) {
+
+
+    LinkedList<String> cars = new LinkedList<String>();
+
+    cars.add("Volvo");
+    cars.add("BMW");
+    cars.add("Ford");
+    cars.add("Mazda");
+    
+
+    System.out.println(cars.getLast());
+
+
+}
+
+
+}
