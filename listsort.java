78b33cd6c83b4629e6c1b7cdcaf4a5bb789a52d1
anand83n <156614503+anand83n@users.noreply.github.com>
2024-08-07 22:07:50 +0530

    Add files via upload

---
+++ listsort.java
@@ -0,0 +1,40 @@
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+
+
+public class listsort {
+
+public static void main(String[] args) {
+
+
+ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+
+
+    myNumbers.add(33);
+    myNumbers.add(15);
+    myNumbers.add(20);
+    myNumbers.add(34);
+    myNumbers.add(8);
+    myNumbers.add(12);
+
+    Collections.sort(myNumbers, Collections.reverseOrder());
+
+    for (int i : myNumbers) {
+
+System.out.println(i);
+
+    }
+
+
+
+
+}
+
+
+
+}
+
+
