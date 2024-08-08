16a2787451c8d2b3108e5b232f7396ba1913b5ba
anand83n <156614503+anand83n@users.noreply.github.com>
2024-08-08 19:26:55 +0530

    Add files via upload

---
+++ hashmp.java
@@ -0,0 +1,41 @@
+
+import java.util.HashMap;
+
+
+public class hashmp {
+
+public static void main(String[] args) {
+
+
+// create a hashmap object called capitalCities
+
+HashMap<String, Integer> people = new HashMap<String, Integer>();
+
+// add keys and values (name , Age)
+
+    people.put("John", 32);
+    people.put("Steve", 30);
+    people.put("Angie", 33);
+
+    for (String i :people.keySet()) {
+
+
+System.out.println(i);
+        
+
+    }
+
+
+        
+
+    }
+
+
+
+
+}
+
+
+
+
+
