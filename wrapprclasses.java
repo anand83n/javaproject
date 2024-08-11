01893f4e117b6ebba92384c4b0ad4ac6208ced22
anand83n <156614503+anand83n@users.noreply.github.com>
2024-08-11 23:54:15 +0530

    Add files via upload

---
+++ wrapprclasses.java
@@ -0,0 +1,16 @@
+public class wrapprclasses {
+
+public static void main(String[] args) {
+
+Integer myInt = 1008;
+
+String myString = myInt.toString();
+
+System.out.println(myString. length());
+
+
+}
+
+
+
+}
