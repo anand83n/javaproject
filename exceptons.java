37dab3ae1ab3a4576cb1dbf9e96df1a83a09c522
anand83n <156614503+anand83n@users.noreply.github.com>
2024-08-17 23:05:31 +0530

    Add files via upload

---
+++ exceptons.java
@@ -0,0 +1,42 @@
+
+class exceptons {
+
+
+public static void main(String [] args)  {
+
+int a = 5;
+int b = 0;
+
+try {
+    
+
+    System.out.println(a / b);
+
+
+} 
+
+catch (Exception E) {
+
+    System.out.println("b is 0");
+  
+
+}
+
+
+System.out.println("Hello");
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+}
